85be976e90550962eccd5558b9ce8db83b632855
Abhishek Pardeshi <abhipardeshi19@gmail.com>
2024-12-09 13:57:39 +0530

    Section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> Getusers()
+    {
+        var users = await context.Users.ToListAsync();
+
+        return users;
+    }
+
+    
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> Getuser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+
+}
